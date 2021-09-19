0f6f7c6d8d98d63e86c203e736890f59f9fb17ac
Christian Decker <christian.decker@reutlingen-university.de>
2021-09-19 22:50:06 +0200

    Basic script-server scripts

---
+++ src/script-server/scripts/funcs.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#
+# Common functions to include in scripts
+#
+# Author: cdeck3r
+#
+
+#
+# draws a horizontal line
+#
+# src:  https://wiki.bash-hackers.org/snipplets/print_horizontal_line#the_parameter_expansion_way
+#
+hr_old() {
+    local start=$'\e(0' end=$'\e(B' line='qqqqqqqqqqqqqqqq'
+    local cols=${COLUMNS:-$(tput cols)}
+    ((cols -= 2)) # modified for use in tap's diag function
+    while ((${#line} < cols)); do line+="$line"; done
+    printf '%s%s%s\n' "$start" "${line:0:cols}" "$end"
+}
+
+#
+# src: https://wiki.bash-hackers.org/snipplets/print_horizontal_line#a_line_across_the_entire_width_of_the_terminal
+#
+hr() {
+    local cols=${COLUMNS:-$(tput cols)}
+    ((cols -= 2)) # modified for use in tap's diag function
+    printf '%*s\n' "${COLUMNS:-$cols}" '' | tr ' ' -
+}
+
+# requires tap-functions.sh
+tool_check() {
+    # check for required tools avail from shell
+    TOOLS=('mosquitto_sub' 'mosquitto_pub' 'tr' 'wc' 'date' 'uniq')
+    for t in "${TOOLS[@]}"; do
+        # check for installed program
+        # Source: https://stackoverflow.com/a/677212
+        command -v "${t}" >/dev/null 2>&1 || { BAIL_OUT "Tool not found: $t"; }
+    done
+    return 0
+}
+
+# tests for tools and pings the MQTT broker
+precheck() {
+    local skip_check=$1
+
+    skip "${skip_check}" "No pre-check required" || {
+        diag "${HR}"
+        diag "Check pre-requisites"
+        diag "${HR}"
+        okx tool_check
+
+        diag "${HR}"
+        diag "${GREEN}[SUCCESS]${NC} - All fine."
+        diag "${HR}"
+        diag " "
+    }
+}
---
+++ src/script-server/scripts/tap-color.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# shellcheck disable=SC1117,SC2027,SC2086
+
+
+#
+# Source: https://gist.github.com/san650/ba500d18179dddb9feb9b3df1d421052
+#
+# Credits belong to the original author
+#
+
+RED=$(printf "\033[31m")
+GREEN=$(printf "\033[32m")
+GRAY=
+CLOSING=$(printf "\033[m")
+
+function color_not_ok() {
+  sed "/^not ok/{; s/^/${RED}/; s/$/${CLOSING}/; }"
+}
+
+function color_ok() {
+  sed "/^ok/{; s/^/"${GREEN}"/; s/$/"${CLOSING}"/; }"
+}
+
+function color_debug() {
+  sed "/^#/{; s/^/"${GRAY}"/; s/$/"${CLOSING}"/; }"
+}
+
+cat \
+  | color_not_ok \
+  | color_ok \
+  | color_debug
